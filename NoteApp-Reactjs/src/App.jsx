bf70774fd77fc83a57c51f6a31fb2956e1a18e50
Adel <adeleslami0077@yahoo.com>
2024-11-24 14:41:17 +0330

    create new component

--- NoteApp-Reactjs/src/App.jsx
+++ NoteApp-Reactjs/src/App.jsx
@@ -1,6 +1,15 @@
+import AddNewNote from "./AddNewNote";
 import "./App.css";
 function App() {
-  return <h1>02: NoteApp starter file</h1>;
+  return (
+    <div className="container">
+        <div className="note-header">note header</div>
+        <div className="note-app">
+            <AddNewNote/>
+            <div className="note-container">notes</div>
+        </div>
+    </div>
+  )
 }
 
 export default App;
