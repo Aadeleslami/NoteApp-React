3df0f461905d432bcb04a24bbd82d20ed1655fab
Adel <adeleslami0077@yahoo.com>
2024-11-28 12:10:20 +0330

    add deleting note

--- NoteApp-Reactjs/src/App.jsx
+++ NoteApp-Reactjs/src/App.jsx
@@ -3,21 +3,25 @@ import "./App.css";
 import NoteList from "./components/NoteList";
 import { useState } from "react";
 function App() {
-  const [notes,setNotes] = useState([]);
+  const [notes, setNotes] = useState([]);
   const handleAddNote = (newNote) => {
-    setNotes(prevNotes => [...prevNotes , newNote])
-  }
+    setNotes((prevNotes) => [...prevNotes, newNote]);
+  };
+  const handleDeletItem = (id) => {
+    // const filtersNote = notes.filter((n) => n.id !== id);
+    setNotes(prevNotes => prevNotes.filter(n => n.id !== id))
+  };
   return (
     <div className="container">
-        <div className="note-header">note header</div>
-        <div className="note-app">
-            <AddNewNote onAddNote={handleAddNote} />
-            <div className="note-container">
-              <NoteList notes={notes}/>
-            </div>
+      <div className="note-header">note header</div>
+      <div className="note-app">
+        <AddNewNote onAddNote={handleAddNote} />
+        <div className="note-container">
+          <NoteList notes={notes} onDeleteNote={handleDeletItem} />
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
--- NoteApp-Reactjs/src/components/NoteList.jsx
+++ NoteApp-Reactjs/src/components/NoteList.jsx
@@ -1,11 +1,40 @@
-
-
-function NoteList({notes}) {
+import { IconTrash } from "@tabler/icons-react";
+function NoteList({ notes , onDeleteNote }) {
   return (
-    <div>
-        <h2>{notes.title}</h2>
+    <div className="note-list">
+      {notes.map((note) => (
+        <NoteItem key={note.id} note={note} onDeleteNote={onDeleteNote} />
+      ))}
     </div>
-  )
+  );
 }
 
-export default NoteList
+export default NoteList;
+function NoteItem({ note , onDeleteNote }) {
+  const options ={
+    year:"numeric",
+    month:"long",
+    day:"numeric"
+  }
+ return(
+  <div className="note-item">
+  <div className="note-item__header">
+    <div>
+      <p className="title">{note.title}</p>
+      <p className="desc">{note.description}</p>
+    </div>
+    <div className="actions">
+      <button onClick={()=> onDeleteNote(note.id)}>
+        <IconTrash className="icon" stroke={2} />
+      </button>
+      <input type="checkbox"  />
+    </div>
+  </div>
+  <div className="note-item__footer">
+    {new Date(note.craetedAt).toLocaleDateString("en-US", options)}
+  </div>
+</div>
+
+ );
+
+}
