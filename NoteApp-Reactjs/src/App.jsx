f29088e6b40920cf3ae9953e9d34b1f9148e4c5e
Adel <adeleslami0077@yahoo.com>
2025-02-07 11:34:24 +0330

    add local storage

--- NoteApp-Reactjs/src/App.jsx
+++ NoteApp-Reactjs/src/App.jsx
@@ -1,13 +1,13 @@
 import AddNewNote from "./components/AddNewNote";
 import "./App.css";
 import NoteList from "./components/NoteList";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import NoteStatus from "./components/NoteStatus";
 import NoteHeader from "./components/NoteHeader";
 function App() {
-  const [notes, setNotes] = useState([]);
+  const [notes, setNotes] = useState(()=> JSON.parse(localStorage.getItem("NOTE"))||[]);
   const [sortBy, setSortBy] = useState("latest");
-
+useEffect(()=>localStorage.setItem("NOTE",JSON.stringify(notes)),[notes])
   const handleAddNote = (newNote) => {
     setNotes((prevNotes) => [...prevNotes, newNote]);
   };
